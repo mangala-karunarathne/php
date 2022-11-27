8797fb74e9cfbc572fecf16cd5bda24c2cd54af4
User <Mangala Karunarathne>
2022-11-27 12:36:57 +0530

    Variables and Strings

---
+++ 2.variables.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variables</title>
+</head>
+<body>
+    <?php 
+    // Variables
+    $variableName = "Value";
+
+    $firstName = "Mangala";
+
+    $lastName = "Karunarathne";
+
+    $age = 30;
+
+    echo $firstName;
+    echo "<br>";
+    echo $lastName;
+    echo "<br>";
+    echo $firstName . " " . $lastName;
+    echo "<br>";
+    echo $firstName . " " . $lastName . " " . $age
+
+    ?>
+</body>
+</html>
---
+++ 3.strings.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strings</title>
+</head>
+<body>
+   <?php
+     $firstName = 'Mangala';
+
+     $lastName = "Karunarathne";
+ 
+ 
+     echo $firstName;
+     echo "<br>";
+     echo $lastName;
+     echo "<br>";
+     echo $firstName . " " . $lastName;
+     echo "<br>";
+     echo $firstName . " " . $lastName;
+     echo "<br>";
+     echo "Full Name: $firstName $lastName";
+     echo "<br>";
+     echo "Full Name: {$firstName} {$lastName}s";
+     echo "<br>";
+     echo "Full Name: $firstName $lastNames";
+
+   ?> 
+</body>
+</html>
